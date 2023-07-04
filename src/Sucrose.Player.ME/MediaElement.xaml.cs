d3d14328f929144163fe7075eb38eca6e20bd7f1
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-07-04 17:02:28 +0300

    Infrastructure Preparation 48

--- src/Sucrose.Player.ME/MediaElement.xaml.cs
+++ src/Sucrose.Player.ME/MediaElement.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows;
 using SPMEEH = Sucrose.Player.ME.Event.Handler;
 using SPMEHMEH = Sucrose.Player.ME.Helper.MediaElementHelper;
 using SPMEMI = Sucrose.Player.ME.Manage.Internal;
@@ -17,7 +20,29 @@ namespace Sucrose.Player.ME
 
             Content = SPMEMI.MediaPlayer;
 
-            SPMEMI.MediaPlayer.Source = new Uri(@"https://prod-streaming-video-msn-com.akamaized.net/ba33094e-efb2-480f-ad77-1a103af156d2/3b215666-f0b7-4538-9ea2-0637d5ed2ea3.mp4", UriKind.RelativeOrAbsolute);
+            string Source = @"https://prod-streaming-video-msn-com.akamaized.net/ba33094e-efb2-480f-ad77-1a103af156d2/3b215666-f0b7-4538-9ea2-0637d5ed2ea3.mp4";
+
+            if (IsUrl(Source))
+            {
+#if NET48_OR_GREATER
+                string LocalSource = @"Video.mp4";
+
+                using HttpClient Client = new();
+                using HttpResponseMessage Response = Client.GetAsync(Source).Result;
+                using Stream Content = Response.Content.ReadAsStreamAsync().Result;
+                using FileStream Stream = new(LocalSource, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+
+                Content.CopyTo(Stream);
+
+                SPMEMI.MediaPlayer.Source = new Uri(@Path.GetFullPath(LocalSource), UriKind.RelativeOrAbsolute);
+#else
+                SPMEMI.MediaPlayer.Source = new Uri(@Source, UriKind.RelativeOrAbsolute);
+#endif
+            }
+            else
+            {
+                SPMEMI.MediaPlayer.Source = new Uri(@Path.GetFullPath(Source), UriKind.RelativeOrAbsolute);
+            }
 
             SPMEMI.MediaPlayer.MediaOpened += SPMEEH.MediaPlayerOpened;
             SPMEMI.MediaPlayer.MediaFailed += SPMEEH.MediaPlayerFailed;
@@ -29,6 +54,15 @@ namespace Sucrose.Player.ME
             SPMEHMEH.SetVolume(100);
 
             SPMEHMEH.Play();
+        }
+
+        public static bool IsUrl(string Address)
+        {
+            string Pattern = @"^(http|https|ftp)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$";
+
+            Regex Regex = new(Pattern, RegexOptions.IgnoreCase);
+
+            return Regex.IsMatch(Address);
         }
     }
 }
