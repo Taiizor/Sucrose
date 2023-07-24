068db7a5a5823006813a413c185e0f9206e2b4aa
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-07-24 03:24:51 +0300

    Infrastructure Preparation 154

--- src/Bundle/Sucrose.Bundle/App.xaml.cs
+++ src/Bundle/Sucrose.Bundle/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
+using SHC = Skylark.Helper.Culture;
 
 namespace Sucrose.Bundle
 {
@@ -7,5 +9,78 @@ namespace Sucrose.Bundle
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+        protected void Close()
+        {
+            Environment.Exit(0);
+            Current.Shutdown();
+            Shutdown();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string Lang = SHC.CurrentUITwoLetterISOLanguageName.ToUpperInvariant();
+
+            if (!CheckLanguage(Lang))
+            {
+                Lang = "EN";
+            }
+
+            ResourceDictionary Resource = new()
+            {
+                Source = new Uri($"/Sucrose.Bundle;component/Properties/Resources_{Lang}.xaml", UriKind.Relative)
+            };
+
+            RemoveResource();
+
+            Current.Resources.MergedDictionaries.Add(Resource);
+
+            base.OnStartup(e);
+        }
+
+        private static bool CheckLanguage(string Lang)
+        {
+            return LoadComponent(new Uri($"/Sucrose.Bundle;component/Properties/Resources_{Lang}.xaml", UriKind.Relative)) is ResourceDictionary;
+        }
+
+        private static void RemoveResource()
+        {
+            List<ResourceDictionary> resourcesToRemove = Current.Resources.MergedDictionaries
+                .Where(Resource => !string.IsNullOrEmpty(Resource.Source?.ToString()) && Resource.Source.ToString().Contains("/Properties/Resources_"))
+                .ToList();
+
+            foreach (ResourceDictionary resource in resourcesToRemove)
+            {
+                Current.Resources.MergedDictionaries.Remove(resource);
+            }
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception Exception = (Exception)e.ExceptionObject;
+
+            if (Exception != null)
+            {
+                MessageBox.Show(Exception.Message + Environment.NewLine + Exception.StackTrace, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+            }
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception Exception = e.Exception;
+
+            if (Exception != null)
+            {
+                MessageBox.Show(Exception.Message + Environment.NewLine + Exception.StackTrace, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+                Close();
+            }
+        }
     }
 }
--- src/Bundle/Sucrose.Bundle/Main.xaml.cs
+++ src/Bundle/Sucrose.Bundle/Main.xaml.cs
@@ -43,31 +43,40 @@ namespace Sucrose.Bundle
             InitializeComponent();
         }
 
-        private static async Task ExtractResources(string SourceFolderPath, string TargetFolderPath)
+        private static void TerminateProcess(string ProcessName)
         {
-            if (!Directory.Exists(TargetFolderPath))
+            IEnumerable<Process> TerminateProcesses = Process.GetProcesses().Where(p => p.ProcessName.Contains(ProcessName) && p.Id != Process.GetCurrentProcess().Id);
+
+            foreach (Process Process in TerminateProcesses)
             {
-                Directory.CreateDirectory(TargetFolderPath);
+                Process.Kill();
             }
-            else
+        }
+
+        private static async Task ExtractDirectory(string ExtractPath)
+        {
+            if (Directory.Exists(ExtractPath))
             {
-                Directory.Delete(TargetFolderPath, recursive: true);
-
-                Directory.CreateDirectory(TargetFolderPath);
+                Directory.Delete(ExtractPath, recursive: true);
             }
 
-            await Task.Delay(MaxDelay);
+            await Task.Delay(MinDelay);
 
+            Directory.CreateDirectory(ExtractPath);
+        }
+
+        private static async Task ExtractResources(string SourcePath, string ExtractPath)
+        {
             Assembly Entry = SHA.Assemble(SEAT.Entry);
             string[] Resources = Entry.GetManifestResourceNames();
 
             foreach (string Resource in Resources)
             {
-                if (Resource.StartsWith($"{SourceFolderPath}\\"))
+                if (Resource.StartsWith($"{SourcePath}\\"))
                 {
-                    string NewResource = Resource.Substring($"{SourceFolderPath}\\".Length);
+                    string Resourcer = Resource.Substring($"{SourcePath}\\".Length);
 
-                    string ExtractFilePath = Path.Combine(TargetFolderPath, NewResource);
+                    string ExtractFilePath = Path.Combine(ExtractPath, Resourcer);
 
                     if (!Directory.Exists(Path.GetDirectoryName(ExtractFilePath)))
                     {
@@ -82,21 +91,15 @@ namespace Sucrose.Bundle
             }
         }
 
-        private static void TerminateProcess(string ProcessName)
-        {
-            IEnumerable<Process> TerminateProcesses = Process.GetProcesses().Where(p => p.ProcessName.Contains(ProcessName) && p.Id != Process.GetCurrentProcess().Id);
-
-            foreach (Process Process in TerminateProcesses)
-            {
-                Process.Kill();
-            }
-        }
-
         private async void Window_ContentRendered(object sender, EventArgs e)
         {
             await Task.Delay(MinDelay);
 
             TerminateProcess(Application);
+
+            await Task.Delay(MaxDelay);
+
+            await ExtractDirectory(Extract);
 
             await Task.Delay(MaxDelay);
 
