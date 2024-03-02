fa1a4f820d7bc2caf8a549c3007fa49636be2989
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-03-02 06:26:43 +0300

    Major Changes

--- src/Library/Sucrose.Memory/Readonly.cs
+++ src/Library/Sucrose.Memory/Readonly.cs
@@ -114,7 +114,7 @@
 
         public static readonly string LauncherMutex = "{Sucrose-Wallpaper-Engine-Launcher}";
 
-        public static readonly string UserAgent = "Sucrose/1.2 (Wallpaper Engine) SucroseWebKit";
+        public static readonly string UserAgent = "Sucrose/1.3 (Wallpaper Engine) SucroseWebKit";
 
         public static readonly string BrowseWebsite = $"https://github.com/{Owner}/{Repository}";
 
